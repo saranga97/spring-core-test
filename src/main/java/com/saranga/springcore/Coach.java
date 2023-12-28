385605556f3c0048fa87d444a3733a20292a6a8e
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2023-12-29 00:00:50 +0530

    Initial commit

---
+++ src/main/java/com/saranga/springcore/Coach.java
@@ -0,0 +1,5 @@
+package com.saranga.springcore;
+
+public interface Coach {
+    String getDailyWorkout();
+}
---
+++ src/main/java/com/saranga/springcore/SpringcoreApplication.java
@@ -0,0 +1,13 @@
+package com.saranga.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoreApplication.class, args);
+	}
+
+}
